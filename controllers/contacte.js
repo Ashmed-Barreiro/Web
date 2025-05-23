f855809b76221f50da573e4925fbabdaaa2acf79
SerraSilvia <silvia.serra.7e6@itb.cat>
2025-05-23 10:31:09 +0200

    fix contacte page

---
+++ controllers/contacte.js
@@ -0,0 +1,4 @@
+/* Marcar pagina en la que estem */
+setTimeout(() => {
+    document.querySelector('#link-contacte').classList.add('pagina-activa');
+}, 150);
---
+++ controllers/home.js
@@ -0,0 +1,4 @@
+/* Marcar pagina en la que estem */
+setTimeout(() => {
+    document.querySelector('#link-home').classList.add('pagina-activa');
+}, 150);
